8d1290fd104e34363947b00ca79b0b73840585eb
juan tumminelli <juantumminelli93@gmail.com>
2026-05-06 09:45:03 -0300

    centramos el formulario

--- pryTumminellilLogin/Form1.Designer.cs
+++ pryTumminellilLogin/Form1.Designer.cs
@@ -140,6 +140,7 @@
             this.Controls.Add(this.txtUsuario);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sinterpart SRL";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
